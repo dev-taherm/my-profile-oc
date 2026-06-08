da3c6a6decd2c60101203707d0fcb1c67e9e5173
taher <tahermahram0@gmail.com>
2026-06-09 00:02:54 +0300

    fix: Button component not forwarding type prop (breaks form submissions)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+import * as React from "react"
 import { Button as ButtonPrimitive } from "@base-ui/react/button"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -44,11 +45,13 @@ function Button({
   className,
   variant = "default",
   size = "default",
+  type = "button",
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: Omit<React.ComponentProps<"button">, "color"> & VariantProps<typeof buttonVariants>) {
   return (
     <ButtonPrimitive
       data-slot="button"
+      type={type}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
